eaabd350c620598a3443995e34e2473e49b0f536
l3miage-patract <theo.patra@yahoo.com>
2023-04-03 21:45:37 +0200

    ajouts images

--- app/src/main/java/fr/uga/l3miage/photonum/CreateBasingObjectForTest.java
+++ app/src/main/java/fr/uga/l3miage/photonum/CreateBasingObjectForTest.java
@@ -8,9 +8,9 @@ public class CreateBasingObjectForTest {
 
     public static Client newClient1() {
         Client client = new Client();
-        client.setNom("Riado");
-        client.setPrenom("Bastien");
-        client.setAdresseMail("bastien.riado@etu.univ-grenoble-ales.fr");
+        client.setNom("monsieur");
+        client.setPrenom("trebuche");
+        client.setAdresseMail("trebuche@outlook.fr");
         client.setAdressePostale("36 rue du tacos");
         client.setMotDePasse("abricot");
         return client;
@@ -18,86 +18,87 @@ public class CreateBasingObjectForTest {
 
     public static Client newClient2() {
         Client client = new Client();
-        client.setNom("Patrac");
-        client.setPrenom("Théo");
-        client.setAdresseMail("theo.patrac@etu.univ-grenoble-ales.fr");
-        client.setAdressePostale("69 avenue tendu");
-        client.setMotDePasse("soral");
+        client.setNom("monsieur");
+        client.setPrenom("trebuche2");
+        client.setAdresseMail("trebuche2@outlook.fr");
+        client.setAdressePostale("36 rue du tacos");
+        client.setMotDePasse("abricot");
         return client;
     }
 
     public static Client newClient3() {
         Client client = new Client();
-        client.setNom("Cormier");
-        client.setPrenom("Levi");
-        client.setAdresseMail("levi.cormier@etu.univ-grenoble-ales.fr");
-        client.setAdressePostale("shibuya avenue");
-        client.setMotDePasse("japon");
+        client.setNom("monsieur");
+        client.setPrenom("trebuche3");
+        client.setAdresseMail("trebuche3@outlook.fr");
+        client.setAdressePostale("36 rue du tacos");
+        client.setMotDePasse("abricot");
         return client;
     }
 
     public static Client newClient4() {
         Client client = new Client();
-        client.setNom("Tornambé");
-        client.setPrenom("damien");
-        client.setAdresseMail("damien.tornambe@etu.univ-grenoble-ales.fr");
-        client.setAdressePostale("66 à droite");
-        client.setMotDePasse("jeanneausecours");
+        client.setNom("monsieur");
+        client.setPrenom("trebuche");
+        client.setAdresseMail("trebuche4@outlook.fr");
+        client.setAdressePostale("36 rue du tacos");
+        client.setMotDePasse("abricot");
         return client;
     }
 
 
     public static Image newImage1() {
         Image image = new Image();
-        image.setChemin("https://zemmour2027.fr/barricades");
-        image.setResolution(666);
-        image.setInfo("Protegez les frontières contre les hordes barbares");
+        image.setChemin("https://test.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
     public static Image newImage2() {
         Image image = new Image();
-        image.setChemin("https://soral.fr/fautlesavoir");
-        image.setResolution(666);
-        image.setInfo("Tout ce qui faut savoir");
+        image.setChemin("https://test2.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
     public static Image newImage3() {
         Image image = new Image();
-        image.setChemin("https://cuisine2000.fr/fourchettes");
-        image.setResolution(666);
+        image.setChemin("https://test3.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
-
     public static Image newImage4() {
         Image image = new Image();
-        image.setChemin("https://jacquieetmichel.fr/fontaine");
-        image.setResolution(666);
-        image.setInfo("Attention ça éclabousse!");
+        image.setChemin("https://test4.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
     public static Image newImage5() {
         Image image = new Image();
-        image.setChemin("https://bigard.fr/pognons");
-        image.setResolution(666);
+        image.setChemin("https://test5.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
     public static Image newImage6() {
         Image image = new Image();
-        image.setChemin("https://akihabara.fr/culottes");
-        image.setResolution(666);
-        image.setInfo("Le meilleur site de culottes sales d'écolières japonaises");
+        image.setChemin("https://test6.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
     public static Image newImage7() {
         Image image = new Image();
-        image.setChemin("https://rouletterusse.fr/arcenciel");
-        image.setResolution(666);
-        image.setInfo("Un jeu mortelle!");
+        image.setChemin("https://test7.gif");
+        image.setResolution(250);
+        image.setInfo("smurl");
         return image;
     }
 
